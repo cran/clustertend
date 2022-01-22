ba2b3fdc4a53edfc5eb8ccc4d12ba2593b4da311
Kevin Wright <kw.stat@gmail.com>
2022-01-22 22:52:46 +0000

    version 1.6

--- NEWS.md
+++ NEWS.md
@@ -1,11 +1,14 @@
+# clustertend 1.6 - Jan 2022
 
-# clustertend 1.5 (Apr 2021)
+The `hopkins()` function is now deprecated in favor of `hopkins::hopkins()`.
+
+# clustertend 1.5 - Apr 2021
 
 New maintainer Kevin Wright.
 
-# clustertend 1.4 (May 2015)
+# clustertend 1.4 - May 2015
 
 First release to CRAN.
 
-Original version written by Luo YiLan, Zeng RuTong <670273197@@qq.com>
+Original version written by Luo YiLan, Zeng RuTong.
 
--- R/hopkins.R
+++ R/hopkins.R
@@ -1,48 +1,83 @@
-hopkins <-
-function(data,n,byrow=F,header=F) 
-{
-  if(is.data.frame(data))
-    data<-as.matrix(data)
-  if (!(is.matrix(data)))
-    stop("data must be data.frame or matrix") 
-  if(n>=nrow(data))
+# hopkins.R
+
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Package `clustertend` is deprecated.  Use package `hopkins` instead.")
+}
+
+## ---------------------------------------------------------------------------
+
+#' @title Calculate the Hopkins' statistic
+#'
+#' Calculate the Hopkins' statistic of given data.
+#'
+#' Note:"Package \code{clustertend} is deprecated.  Use package \code{hopkins} instead.
+#' 
+#' Sample data must be preprocessed into dataframe or matrix form before given as the value of parameter "data".
+#'
+#' @param X Data (matrix or data.frame) to check clusterability.
+#' 
+#' @param n The number of rows to sample from X. The default is 1/10th the number of rows of X.
+#' 
+#' @return The value returned is actually 1-Hopkins statistic.
+#' 
+#' @author Luo YiLan, Zeng RuTong.
+#' 
+#' @examples
+#' set.seed(1)
+#' hopkins(iris[,-5], n=15)
+#' 
+#' @references 
+#' Lawson, R.G. and Jurs, P.C.(1990).
+#' New index for clustering tendency and its application to chemical problems.
+#' Journal of Chemical Information and Computer Sciences. 30(1):36-41.
+#' 
+#' @importFrom stats dist runif
+#' @export
+#' 
+hopkins <- function(X,n=ceiling(nrow(X)/10)) {
+  .Deprecated(msg="Package `clustertend` is deprecated.  Use package `hopkins` instead.")
+
+  if(is.data.frame(X))
+    X <- as.matrix(X)
+  if (!(is.matrix(X)))
+    stop("X must be data.frame or matrix")
+  
+  if(n>=nrow(X))
     stop("n must be no larger than num of samples")
-  if(byrow==T) data<-t(data)
-  if(header==T) data<-data[-1,]
-  c<-apply(data,2,min)#minimum value per colume
-  d<-apply(data,2,max)
-  p<-matrix(0,ncol=ncol(data),nrow=n)#n vectors of space
-  for(i in 1:ncol(data))
-  {
-    p[,i]<-runif(n,min=c[i],max=d[i])
+  c <- apply(X,2,min) # minimum value per colume
+  d <- apply(X,2,max)
+  p <- matrix(0,ncol=ncol(X),nrow=n) # n vectors of space
+  for(i in 1:ncol(X)) {
+    p[,i] <- runif(n,min=c[i],max=d[i])
   }
-  k<-round(runif(n,1,nrow(data)))
-  q<-as.matrix(data[k,])
-  distp=rep(0,nrow(data))
-  #distq=rep(0,nrow(data)-1)
-  distq=0;
-  minp=rep(0,n)
-  minq=rep(0,n)
-  for(i in 1:n)
-  {
-    distp[1]<-dist(rbind(p[i,],data[1,]))
-    minqi<-dist(rbind(q[i,],data[1,]))
-    for(j in 2:nrow(data))
-    {
-      distp[j]<-dist(rbind(p[i,],data[j,]))
-      error<-q[i,]-data[j,]
-      if(sum(abs(error))!=0)
-      {
-        #distq[j]<-dist(rbind(q[i,],data[j,]))
-        distq<-dist(rbind(q[i,],data[j,]))
+
+  #k <- round(runif(n,1,nrow(X)))
+  k <- sample(1:nrow(X), n)
+  
+  q <- as.matrix(X[k,])
+  distp <- rep(0,nrow(X))
+  #distq=rep(0,nrow(X)-1)
+  distq <- 0;
+  minp <- rep(0,n)
+  minq <- rep(0,n)
+  for(i in 1:n) {
+    distp[1] <- dist(rbind(p[i,],X[1,]))
+    minqi <- dist(rbind(q[i,],X[1,]))
+    for(j in 2:nrow(X)) {
+      distp[j] <- dist(rbind(p[i,],X[j,]))
+      error <- q[i,]-X[j,]
+      if(sum(abs(error))!=0) {
+        #distq[j] <- dist(rbind(q[i,],X[j,]))
+        distq <- dist(rbind(q[i,],X[j,]))
         if(distq<minqi)
-          minqi<-distq;
+          minqi <- distq;
       }
     }
-    minp[i]<-min(distp)
-   # minq[i]<-apply(distq,1,min)
-   minq[i]<-minqi;
+    minp[i] <- min(distp)
+   # minq[i] <- apply(distq,1,min)
+   minq[i] <- minqi;
   }
   list(H=(sum(minq)/(sum(minp)+sum(minq))))
   
 }
+
